6ba67341a225db236611a1cb65035c8f06e2be13
rougeTitan <63300308+rougeTitan@users.noreply.github.com>
2023-11-03 16:14:04 -0400

    adding booking component

--- ionic-navigation-demo-app/src/app/bookings/booking.service.ts
+++ ionic-navigation-demo-app/src/app/bookings/booking.service.ts
@@ -1,20 +1,142 @@
-import { Injectable } from "@angular/core";
-import {Booking} from './booking.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, from } from 'rxjs';
+import { take, tap, delay, switchMap, map } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
-export class BookingService{
-    private _bookings: Booking[] =[
-        {
-            id: 'xyz',
-            placeId:'p1',
-            placeTitle: 'Manhattan Mansion',
-            guestNumber: 2,
-            userId:'abc'
+import { Booking } from './booking.model';
+import { AuthService } from '../auth/auth.service';
+
+interface BookingData {
+  bookedForm: string;
+  bookedTo: string;
+  firstName: string;
+  guestNumber: number;
+  lastName: string;
+  placeId: string;
+  placeImage: string;
+  placeTitle: string;
+  userId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class BookingService {
+  private _bookings = new BehaviorSubject<Booking[]>([]);
+
+  get bookings() {
+    return this._bookings.asObservable();
+  }
+
+  constructor(private authService: AuthService, private http: HttpClient) {}
+
+  addBooking(
+    placeId: string,
+    placeTitle: string,
+    placeImage: string,
+    firstName: string,
+    lastName: string,
+    guestNumber: number,
+    dateFrom: Date,
+    dateTo: Date
+  ) {
+    let generatedId: string;
+    let newBooking: Booking;
+    let fetchedUserId: string;
+    return this.authService.userId.pipe(
+      take(1),
+      switchMap((userId) => {
+        if (!userId) {
+          throw new Error('No userId found!');
         }
-    ];
+        fetchedUserId = userId;
+        return this.authService.token;
+      }),
+      take(1),
+      switchMap((token) => {
+        newBooking = new Booking(
+          Math.random().toString(),
+          placeId,
+          fetchedUserId,
+          placeTitle,
+          placeImage,
+          firstName,
+          lastName,
+          guestNumber,
+          dateFrom,
+          dateTo
+        );
+        return this.http.post<{ name: string }>(``, {
+          ...newBooking,
+          id: null,
+        });
+      }),
+      switchMap((resData) => {
+        generatedId = resData.name;
+        return this.bookings;
+      }),
+      take(1),
+      tap((bookings) => {
+        newBooking.id = generatedId;
+        this._bookings.next(bookings.concat(newBooking));
+      })
+    );
+  }
 
-    get bookings() {
-        //return fresh copy of the array
-        return [...this._bookings];
-    }
+  cancelBooking(bookingId: string) {
+    return this.authService.token.pipe(
+      take(1),
+      switchMap((token) => {
+        return this.http.delete(``);
+      }),
+      switchMap(() => {
+        return this.bookings;
+      }),
+      take(1),
+      tap((bookings) => {
+        this._bookings.next(bookings.filter((b) => b.id !== bookingId));
+      })
+    );
+  }
+
+  fetchBookings() {
+    let fetchedUserId: string;
+    return this.authService.userId.pipe(
+      take(1),
+      switchMap((userId) => {
+        if (!userId) {
+          throw new Error('User not found!');
+        }
+        fetchedUserId = userId;
+        return this.authService.token;
+      }),
+      take(1),
+      switchMap((token) => {
+        return this.http.get<{ [key: string]: BookingData }>(``);
+      }),
+      map((bookingData) => {
+        const bookings = [];
+        for (const key in bookingData) {
+          if (bookingData.hasOwnProperty(key)) {
+            bookings.push(
+              new Booking(
+                key,
+                bookingData[key].placeId,
+                bookingData[key].userId,
+                bookingData[key].placeTitle,
+                bookingData[key].placeImage,
+                bookingData[key].firstName,
+                bookingData[key].lastName,
+                bookingData[key].guestNumber,
+                new Date(bookingData[key].bookedForm),
+                new Date(bookingData[key].bookedTo)
+              )
+            );
+          }
+        }
+        return bookings;
+      }),
+      tap((bookings) => {
+        this._bookings.next(bookings);
+      })
+    );
+  }
 }
--- ionic-navigation-demo-app/src/app/bookings/create-booking/create-booking.component.ts
+++ ionic-navigation-demo-app/src/app/bookings/create-booking/create-booking.component.ts
@@ -1,29 +1,69 @@
-import { Component, Input, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-
-import{Place} from '../../places/place.model';
+import { NgForm } from '@angular/forms';
+import { Place } from '../../places/place.model';
 
 @Component({
   selector: 'app-create-booking',
   templateUrl: './create-booking.component.html',
   styleUrls: ['./create-booking.component.scss'],
 })
+export class CreateBookingComponent implements OnInit {
+  @Input() selectedPlace: Place;
+  @Input() selectedMode: 'select' | 'random';
+  @ViewChild(`f`, { static: true }) from: NgForm;
+  startDate: string;
+  endDate: string;
 
-export class CreateBookingComponent  implements OnInit {
- @Input() selectedPlace: Place;
+  constructor(private modalCtrl: ModalController) {}
 
-  constructor(private modalCtrl:ModalController) { }
-
-  ngOnInit() {}
-
-  onCancel() {
-    this.modalCtrl.dismiss(null,'cancel');
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace.availableFrom);
+    const availableTo = new Date(this.selectedPlace.availableTo);
+    if (this.selectedMode == 'random') {
+      this.startDate = new Date(
+        availableFrom.getTime() +
+          Math.random() *
+            (availableTo.getTime() -
+              7 * 24 * 60 * 60 * 1000 -
+              availableFrom.getTime())
+      ).toString();
+      this.endDate = new Date(
+        new Date(this.startDate).getTime() +
+          Math.random() *
+            (new Date(this.startDate).getTime() +
+              6 * 24 * 60 * 60 * 1000 -
+              new Date(this.startDate).getTime())
+      ).toString();
+    }
   }
 
-  onBookPlace(){
-    this.modalCtrl.dismiss({
-      message: 'this is a dummy message!'},'confirm');
+  onCancel() {
+    this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  onBookPlace() {
+    if (!this.from.valid || !this.datesValid) {
+      return;
     }
-  
+
+    this.modalCtrl.dismiss(
+      {
+        bookingData: {
+          firstName: this.from.value['first-name'],
+          lastName: this.from.value['last-name'],
+          guestNumber: this.from.value['guest-number'],
+          startDate: this.from.value['date-form'],
+          endDate: new Date(this.from.value['date-to']),
+        },
+      },
+      'confirm'
+    );
+  }
+
+  datesValid() {
+    const startDate = new Date(this.from.value['date-form']);
+    const endDate = new Date(this.from.value['date-to']);
+    return endDate > startDate;
+  }
 }
